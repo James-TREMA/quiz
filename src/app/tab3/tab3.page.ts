afa62fd5d3d4959485bd7fd9270f3936eed60b57
James-Trema <andrdu29@live.fr>
2024-11-26 22:53:54 +0100

    up

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,14 +1,32 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonCardHeader, IonCardContent, IonCard, IonButton, IonCardTitle } from '@ionic/angular/standalone';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent],
+  imports: [IonCardTitle, IonButton, IonCard, IonCardContent, IonCardHeader, IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent, CommonModule],
 })
 export class Tab3Page {
+  profileImage: string = ''; // URL de l'image de profil
+  totalAnswers: number = 0; // Total de réponses
+  correctAnswers: number = 0; // Réponses correctes
+  incorrectAnswers: number = 0; // Réponses incorrectes
+
   constructor() {}
+
+  // Méthode pour charger une image de profil
+  onProfileImageChange(event: any) {
+    const file = event.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = (e: any) => {
+        this.profileImage = e.target.result;
+      };
+      reader.readAsDataURL(file);
+    }
+  }
 }
